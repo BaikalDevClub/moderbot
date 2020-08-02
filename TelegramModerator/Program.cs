530a3baab30899097ddb220a9ddb6c9379f1b763
MyOnAsSalat <89041155523@yandex.ru>
2020-08-02 18:22:46 +0800

    Add project files.

---
+++ TelegramModerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Telegram;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramModerator
+{
+    class Program
+    {
+        static TelegramBotClient bot;
+        static void Main(string[] args)
+        {
+            bot = new TelegramBotClient("");
+            bot.OnMessage += OnMessage;
+        }
+
+        private static void OnMessage(object sender, MessageEventArgs e)
+        {
+            bot.SendTextMessageAsync(e.Message.Chat.Id,"Hello, " + e.Message.Chat.Username);
+        }
+    }
+}
